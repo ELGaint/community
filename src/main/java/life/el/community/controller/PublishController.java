2f6ffc9df5bb7ff62240a0892c7a5d630238fcf9
EL <1317150334@qq.com>
2019-07-09 16:17:58 +0800

    ”添加发布功能实现代码“

--- src/main/java/life/el/community/controller/PublishController.java
+++ src/main/java/life/el/community/controller/PublishController.java
@@ -1,13 +1,85 @@
 package life.el.community.controller;
 
+import life.el.community.mapper.QuestionMapper;
+import life.el.community.mapper.UserMapper;
+import life.el.community.model.Question;
+import life.el.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
 
+    @Autowired
+    QuestionMapper questionMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPulish(
+            @RequestParam("title") String title,
+            @RequestParam("description") String description,
+            @RequestParam("tag") String tag,
+            HttpServletRequest request,
+            Model model) {
+        //保存信息在页面展示
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        //校验标题，内容，标签不为空
+        if (StringUtils.isBlank(title)) {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (StringUtils.isBlank(description)) {
+            model.addAttribute("error", "内容不能为空");
+            return "publish";
+        }
+        if (StringUtils.isBlank(tag)) {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+        //获取用户信息
+        User user = null;
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+        //用户信息为空，未登录，返回发布页面并展示错误信息
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+        //保存发布的问题信息
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(System.currentTimeMillis());
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
